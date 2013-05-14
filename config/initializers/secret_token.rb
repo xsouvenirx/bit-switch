a3c2544601b10909ee71c58126826086e5843b10
souvenir <xsouvenirx@gmail.com>
2013-05-14 00:20:08 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitSwitch::Application.config.secret_token = '0344b0880cff98c757080c06202a937f56ec1d4128a43eb6e9ff576aba9a708b35efe7d63c3c1b53dc68be4e959e8fda79228f49f611081c82df268c9709a3a2'
